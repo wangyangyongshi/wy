8ed5a8791964ecbfb7606fe275749b2861c0bf3c
wangyang <735847142@qq.com>
2019-03-29 23:36:44 +0800

    Initial commit

---
+++ src/main/java/com/page/dao/MovieDao.java
@@ -0,0 +1,16 @@
+package com.page.dao;
+
+import com.page.Entity.Movie;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author 王二小
+ * @date 2019/3/28 0:11
+ */
+@MapperScan
+public interface MovieDao {
+
+    Movie findOne();
+}
